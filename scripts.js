abc751189673f4453934623426b0b4e0664eea3c
Misato11 <69413126+Misato11@users.noreply.github.com>
2025-01-03 16:46:51 +0300

    Delete scripts.js

--- scripts.js
+++
@@ -1,7 +0,0 @@
-
-// Скрипт для взаимодействия
-document.querySelectorAll('.btn').forEach(button => {
-    button.addEventListener('click', () => {
-        alert('Спасибо за интерес к нашему продукту!');
-    });
-});
